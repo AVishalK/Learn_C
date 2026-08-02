f405b409af41370ed1cc79816ce62aa625693bfc
Vishal Kumar <65348062+AVishalK@users.noreply.github.com>
2026-08-02 21:30:55 +0530

    Create sum_of_elements.c

---
+++ my-folder/sum_of_elements.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int arr[10],i,sum=0;
+for (i=0; i<10; i++)
+{
+printf("enter the number for arr[%d]", i);
+scanf("%d",&arr[i]);
+sum =sum+arr[i];
+}
+printf("sum = %d",sum);
+}
+  
